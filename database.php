575e75dad3389b7d5fdd79fcb22a436fa3732c9c
AlexKrupenkin <arturm.m@bk.ru>
2021-03-23 21:07:05 +0300

    db

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+
+$link = mysqli_connect('localhost', 'root', '', 'fp');
+
+
+if (mysqli_connect_errno()) {
+    echo 'error connect (' . mysqli_connect_errno() . ')';
+    exit();
+}
+
+
+$sql = "SELECT * FROM `users`";
+
+$result = mysqli_query($link, $sql);
+
+$users = mysqli_fetch_all;
+
+
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
